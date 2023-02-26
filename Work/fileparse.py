4b0105a99c703b047cbca9fb23955a5e38fd11f5
ktsgj <ktsgj96@gmail.com>
2023-02-26 21:30:40 +0900

    2/26 course 03_6 Completed

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,45 +3,87 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
+# def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
+#     '''
+#     CSV 파일을 파싱해 레코드의 목록을 생성
+#     '''
+#     with open(filename) as f:
+#         rows = csv.reader(f, delimiter=delimiter)
+#
+#         if select and not has_headers:
+#             raise RuntimeError("select argument requires column headers")
+#
+#         # 헤더를 읽음
+#         if has_headers:
+#             headers = next(rows)
+#         else:
+#             headers = []
+#
+#         if select:
+#             indices = [headers.index(col) for col in select]
+#             headers = select
+#         else:
+#             indices = []
+#
+#         records = []
+#         for idx, row in enumerate(rows):
+#             try:
+#                 if not row:    # 데이터가 없는 행을 건너뜀
+#                     continue
+#                 if indices:
+#                     row = [row[index] for index in indices]
+#                 if types:
+#                     row = [func(val) for func, val in zip(types, row)]
+#                 if headers:
+#                     record = dict(zip(headers, row))
+#                 else:
+#                     record = tuple(row)
+#                 records.append(record)
+#             except ValueError as e:
+#                 if silence_errors:
+#                     print(f"Row {idx+1}: Couldn't convert {row}")
+#                     print(f"Row {idx+1}: Reason {e}")
+#
+#     return records
+
+def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     '''
     CSV 파일을 파싱해 레코드의 목록을 생성
     '''
-    with open(filename) as f:
-        rows = csv.reader(f, delimiter=delimiter)
+    rows = csv.reader(lines, delimiter=delimiter)
 
-        if select and not has_headers:
-            raise RuntimeError("select argument requires column headers")
+    if select and not has_headers:
+        raise RuntimeError("select argument requires column headers")
 
-        # 헤더를 읽음
-        if has_headers:
-            headers = next(rows)
-        else:
-            headers = []
+    # 헤더를 읽음
+    if has_headers:
+        headers = next(rows)
+    else:
+        headers = []
 
-        if select:
-            indices = [headers.index(col) for col in select]
-            headers = select
-        else:
-            indices = []
+    if select:
+        indices = [headers.index(col) for col in select]
+        headers = select
+    else:
+        indices = []
 
-        records = []
-        for idx, row in enumerate(rows):
-            try:
-                if not row:    # 데이터가 없는 행을 건너뜀
-                    continue
-                if indices:
-                    row = [row[index] for index in indices]
-                if types:
-                    row = [func(val) for func, val in zip(types, row)]
-                if headers:
-                    record = dict(zip(headers, row))
-                else:
-                    record = tuple(row)
-                records.append(record)
-            except ValueError as e:
-                if silence_errors:
-                    print(f"Row {idx+1}: Couldn't convert {row}")
-                    print(f"Row {idx+1}: Reason {e}")
+    records = []
+    for idx, row in enumerate(rows):
+        try:
+            if not row:    # 데이터가 없는 행을 건너뜀
+                continue
+            if indices:
+                row = [row[index] for index in indices]
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
+        except ValueError as e:
+            if silence_errors:
+                print(f"Row {idx+1}: Couldn't convert {row}")
+                print(f"Row {idx+1}: Reason {e}")
 
     return records
--- Work/report.py
+++ Work/report.py
@@ -151,11 +151,13 @@ from fileparse import parse_csv
 
 def read_portfolio(filename):
     '''포트폴리오 파일의 총 비용(주식수*가격)을 계산'''
-    return parse_csv(filename, select=['name','shares','price'], types=[str,int,float])
+    with open(filename) as lines:
+        return parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
 
 def read_prices(filename):
     '''포트폴리오 파일의 총 비용(주식수*가격)을 계산'''
-    return dict(parse_csv(filename, types=[str,float], has_headers=False))
+    with open(filename) as lines:
+        return dict(parse_csv(lines, types=[str,float], has_headers=False))
 
 def make_report(portfolio, prices):
     report = []
